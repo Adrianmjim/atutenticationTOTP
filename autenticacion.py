82d178b9291c1acae6d1bb13b16c0c3dd7fc686a
adrianmjimuni <adrima05@ucm.es>
2017-01-15 14:43:22 +0100

    AMBAS PARTES 100%

--- autenticacion.py
+++ autenticacion.py
@@ -8,8 +8,10 @@
 from bottle import run, post, request
 # Resto de importaciones
 from pymongo import MongoClient
-import hashlib
+import hashlib 
 import utils
+import onetimepass
+import urllib
 ##############
 # APARTADO 1 #
 ##############
@@ -90,30 +92,64 @@ def login():
 def gen_secret():
     # >>> gen_secret()
     # '7ZVVBSKR22ATNU26'
-    pass
+    return utils.secretGenerator()
     
     
 def gen_gauth_url(app_name, username, secret):
     # >>> gen_gauth_url( 'GIW_grupoX', 'pepe_lopez', 'JBSWY3DPEHPK3PXP')
     # 'otpauth://totp/pepe_lopez?secret=JBSWY3DPEHPK3PXP&issuer=GIW_grupoX
-    pass
+    return "otpauth://totp/"+username+"?secret="+secret+"&issuer="+app_name
         
 
 def gen_qrcode_url(gauth_url):  
     # >>> gen_qrcode_url('otpauth://totp/pepe_lopez?secret=JBSWY3DPEHPK3PXP&issuer=GIW_grupoX')
     # 'http://api.qrserver.com/v1/create-qr-code/?data=otpauth%3A%2F%2Ftotp%2Fpepe_lopez%3Fsecret%3DJBSWY3DPEHPK3PXP%26issuer%3DGIW_grupoX'
-    pass
+    return "http://api.qrserver.com/v1/create-qr-code/?data="+gauth_url
     
 
 
 @post('/signup_totp')
 def signup_totp():
-    pass
-        
+    nickname = request.forms.get('nickname')
+    name = request.forms.get('name')
+    country = request.forms.get('country')
+    email = request.forms.get('email')
+    password = request.forms.get('password')
+    password2 = request.forms.get('password2')
+    if password != password2:
+        return "<b>Las contraseñas no coinciden</b>"
+    elif utils.getUser(nickname) != None:
+        print utils.getUser(nickname)
+        return "<b>El alias del usuario ya existe</b>"
+    salt = utils.saltGenerator(len(password))
+    m = hashlib.sha256()
+    m.update(password)
+    m.update(salt)
+    m.update("123456789")
+    password = m.hexdigest()
+    secret = gen_secret()
+    utils.insertUserTotp(nickname,name,country,email,password,salt,secret)
+    qr = urllib.urlopen(gen_qrcode_url(gen_gauth_url("GIW_grupo13", nickname, secret))).read()
+    return "<b>"+nickname+ " "+secret+"</b>" + qr 
+    
         
 @post('/login_totp')        
 def login_totp():
-    pass
+    nickname = request.forms.get('nickname')
+    password = request.forms.get('password')
+    totp= request.forms.get('totp')
+    user = utils.getUser(nickname)
+    if user == None:
+        return "<b>Usuario o contraseña incorrectos</b>"
+    m = hashlib.sha256()
+    m.update(password)
+    m.update(user['salt'])
+    m.update("123456789")
+    auxPassword = m.hexdigest()
+    if auxPassword == user['password'] and onetimepass.valid_totp(token=totp, secret=user['secret']):
+        return "<b>Bienvenido "+user['name']+"</b>"
+    else:
+        return "<b>Usuario o contraseña incorrectos</b>"
 
     
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ Created on Sat Jan 14 20:32:46 2017
 """
 from pymongo import MongoClient
 import uuid
+import random
 def saltGenerator(longi):
     ale=uuid.uuid4().hex
     while(len(ale)<longi):
@@ -38,3 +39,18 @@ def getUser(nickName):
 def modifyUser(nickName,passW,salt):
     db=getDB()
     db.users.update({"_id":nickName},{"$set":{"password":passW, "salt":salt}})
+def secretGenerator():
+    claves = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z',26:'2',27:'3',28:'4',29:'5',30:'6',31:'7'}
+    salida = ""
+    for i in range(16):
+        salida +=claves[random.randint(0,31)]
+    return salida
+def insertUserTotp(nickName,name,email,country,password,salt,secret):
+    db=getDB()
+    db.users.insert_one({"name":name,
+                     "_id":nickName,
+                     "email":email,
+                     "country":country,
+                     "password":password,
+                     "salt":salt,
+                     "secret":secret})
